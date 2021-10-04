0cd426e2875be6b12a4d242965726b8ee2c1decd
Shane Strachan <shane@Shanes-MacBook-Pro-2.local>
2021-10-04 13:27:13 -0700

    Modified commits

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  webpack5: false,
-  webpack: (config) => {
-    config.node = {
-      dns: "mock",
-      fs: "empty",
-      path: true,
-      url: false,
-    };
-    return config;
-  },
-};
+  // Webpack 5 is enabled by default
+  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+  webpack5: true,
+}
